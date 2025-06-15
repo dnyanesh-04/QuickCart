e88b4671631c44e9c3ad34ac5bc233fdf55e02b4
Dnyanesh Mohite <dnyaneshmohite.4104@gmail.com>
2025-06-15 19:24:32 +0530

    Add syncUserCreation function for Clerk user creation event

--- config/inngest.js
+++ config/inngest.js
@@ -4,6 +4,23 @@ import User from '@/models/user';
 
 export const inngest = new Inngest({ name: 'QuickCart' });
 
+// Create user function (this part was missing)
+export const syncUserCreation = inngest.createFunction(
+    { id: 'create-user-with-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+        };
+        await connectDB();
+        await User.create(userData);
+    }
+);
+
 export const syncUserUpdation = inngest.createFunction(
     { id: 'update-user-with-clerk' },
     { event: 'clerk/user.updated' },
