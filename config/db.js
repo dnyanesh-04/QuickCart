a3b34e45914741615867bb2b80b59238203bafb3
Dnyanesh Mohite <dnyaneshmohite.4104@gmail.com>
2025-06-15 18:35:14 +0530

    Fix db.js for Vercel build compatibility

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+let cached = globalThis.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = globalThis.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
